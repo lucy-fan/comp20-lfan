2828931137a9569af37d92b22e3a0148183c581d
Lucy Fan <LucyFan@Lucys-MacBook-Pro.local>
2018-06-18 19:21:37 -0700

    xmlhttprequest works

--- messages/README.md
+++ messages/README.md
@@ -1,4 +1,8 @@
-I've successfully implemented the JSON parsing that goes on the page, as well as successfully being able to insert the line break between the two messages.
+I've successfully implemented the JSON parsing that goes on the page, as well as successfully being able to insert the line break between the two messages. 
+
+Successfully used XMLHTTPRequest to get the JSON data and parsed it.
+
+CSS does not work quite yet. Am working on it.
 
 I worked on this code alone.
 
--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,13 @@
-// Your JavaScript goes here...
+function parse() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    	var jsonData = this.responseText;
+    	var parsed = JSON.parse(jsonData);
+      document.getElementById("messages").innerHTML = parsed[0]["content"] + " " + parsed[0]["username"] + "<br>" + parsed[1]["content"] + " " + parsed[1]["username"];
+    }
+  };
+ xhttp.open("GET", "data.json", true);
+ xhttp.send();
+
+}
